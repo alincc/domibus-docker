5da91eed0300be2a1c58eaa608b935ba7676caaf
baciuco <cosmin.baciu@ext.ec.europa.eu>
2017-12-06 13:27:55 +0100

    [EDELIVERY-2950] - Clean the Tomcat Docker scripts

--- dockerbuild/build/domibus/130-tomcat/edelivery-tomcat-8.0.39.Dockerfile
+++ dockerbuild/build/domibus/130-tomcat/edelivery-tomcat-8.0.39.Dockerfile
@@ -34,5 +34,6 @@ EXPOSE 8080
 RUN echo 'ls -la $CATALINA_HOME'
 RUN ls -la $CATALINA_HOME
 
-ENTRYPOINT /data/tomcat/entrypoint.sh $CATALINA_HOME
+#ENTRYPOINT /data/tomcat/entrypoint.sh $CATALINA_HOME
+ENTRYPOINT ["$CATALINA_HOME/entrypoint.sh", "$CATALINA_HOME"]
 
--- dockerbuild/build/domibus/130-tomcat/entrypoint.sh
+++ dockerbuild/build/domibus/130-tomcat/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-echo ; echo "Sourcing domInstall Common Functions"
+
 CATALINA_HOME=$1
+echo ; echo "-------CATALINA_HOME $CATALINA_HOME"
 export CATALINA_HOME=$CATALINA_HOME
 
 function startDomibus {
-   displayFunctionBanner ${FUNCNAME[0]}
+   #displayFunctionBanner ${FUNCNAME[0]}
 
    echo ; echo "Starting Domibus - Tomcat: $CATALINA_HOME/bin/catalina.sh start"
    nohup $CATALINA_HOME/bin/catalina.sh start > $CATALINA_HOME/domibus.log 2>&1 &
