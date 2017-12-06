d82763ce77789fba1734ff1be9be3f14155f7174
baciuco <cosmin.baciu@ext.ec.europa.eu>
2017-12-06 13:44:09 +0100

    [EDELIVERY-2950] - Clean the Tomcat Docker scripts

--- dockerbuild/build/domibus/130-tomcat/entrypoint.sh
+++ dockerbuild/build/domibus/130-tomcat/entrypoint.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-
 CATALINA_HOME=$1
 echo ; echo "-------CATALINA_HOME $CATALINA_HOME"
 export CATALINA_HOME=$CATALINA_HOME
-
-function startDomibus {
-   #displayFunctionBanner ${FUNCNAME[0]}
-
-   echo ; echo "Starting Domibus - Tomcat: $CATALINA_HOME/bin/catalina.sh start"
-   nohup $CATALINA_HOME/bin/catalina.sh start > $CATALINA_HOME/domibus.log 2>&1 &
-}
+echo ; echo "Starting Domibus - Tomcat: $CATALINA_HOME/bin/catalina.sh start"
+nohup $CATALINA_HOME/bin/catalina.sh start > $CATALINA_HOME/domibus.log 2>&1 &
 
 
-##########################################################################
-# MAIN PROGRAMM STARTS HERE
-##########################################################################
-
-startDomibus
-#tail -f $CATALINA_HOME/logs/catalina.out
 
 
