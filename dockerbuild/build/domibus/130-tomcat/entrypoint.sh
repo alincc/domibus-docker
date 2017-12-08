37c705622a430c33131e6320023e176115e125a0
baciuco <cosmin.baciu@ext.ec.europa.eu>
2017-12-08 15:21:53 +0100

    [EDELIVERY-2950] - Clean the Tomcat Docker scripts

--- dockerbuild/build/domibus/130-tomcat/entrypoint.sh
+++ dockerbuild/build/domibus/130-tomcat/entrypoint.sh
@@ -6,7 +6,7 @@ echo ; echo "-------CATALINA_HOME $CATALINA_HOME"
 echo ; echo "Starting Domibus - Tomcat: $CATALINA_HOME/bin/catalina.sh start"
 nohup $CATALINA_HOME/bin/catalina.sh start > $CATALINA_HOME/domibus.log 2>&1 &
 
-tail -f $CATALINA_HOME/domibus.log
+#tail -f $CATALINA_HOME/domibus.log
 
 
 
