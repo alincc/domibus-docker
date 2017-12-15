515e3ecf0fe90d734599526a77325a1d21abd4e0
baciuco <cosmin.baciu@ext.ec.europa.eu>
2017-12-15 09:34:36 +0100

    [EDELIVERY-2950] - Clean the Tomcat Docker scripts

--- dockerbuild/build/domibus/domibus-mysql/go.sh
+++ dockerbuild/build/domibus/domibus-mysql/go.sh
@@ -20,16 +20,8 @@ SQL_SCRIPTS_DISTRIBUTION_NAME=domibus-distribution-${DOMIBUS_VERSION}-sql-script
 SQL_SCRIPTS_DISTRIBUTION=${DOMIBUS_DISTRIBUTION}/${SQL_SCRIPTS_DISTRIBUTION_NAME}
 
 rm -rf  ${LOCAL_ARTEFACTS} ; mkdir -p ${LOCAL_ARTEFACTS}/sql-scripts
-
-#. ../../../domInstall/scripts/functions/getDomibus.functions
-#. ../../../domInstall/scripts/functions/common.functions
-
-#getDomibusSQLScripts $1 ./temp/sql-scripts
-
 unzip ${SQL_SCRIPTS_DISTRIBUTION} -d ${LOCAL_ARTEFACTS}
 
-#zipFilename="`ls -1 ${LOCAL_ARTEFACTS}/sql-scripts`"
-#echo ; echo "Getting ZIP file name: ${zipFilename}"
 
 SQLDatabaseInitScriptName="`ls -1 ${LOCAL_ARTEFACTS}/sql-scripts | grep mysql | grep -v migration`"
 echo ; echo "Database schema SQL creation nameis:" ${SQLDatabaseInitScriptName}
@@ -41,8 +33,6 @@ dockerBuildContext="${WORKING_DIR}"
 dockerFile="`ls -1 ${WORKING_DIR}/*.Dockerfile`"
 dockerImage=domibus-mysql:${domibusVersionLowerCase}
 DockerBuildArgs="--build-arg DOMIBUS_SCHEMA=${SQLDatabaseInitScript}"
-
-
 
 echo
 echo "Building Docker Image: ${dockerImage}:"
@@ -58,9 +48,7 @@ echo
 echo "   - Command				: ${dockerBuildCmd}"
 echo
 
-#eval ${dockerBuildCmd}
-
-#rm -rf ${WORKING_DIR}/temp
+eval ${dockerBuildCmd}
 
 exit
 
