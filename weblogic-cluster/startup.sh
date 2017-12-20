2cecfc9f6887e0f3004e41b75378eddef2830258
FERNANDES Henrique <hfernandes@linkare.com>
2017-12-20 15:41:09 +0000

    EDELIVERY-2969 - Adding Apache HTTPD Load Balancer image

--- weblogic-cluster/startup.sh
+++ weblogic-cluster/startup.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env bash
 
 BASE=$(pwd)
-RESOURCES_REPO=${BASE}/images
 
 export DOMIBUS_VERSION=4.0-SNAPSHOT
 export DOMIBUS_SHORT_VERSION=4.0
 
-# Build all images
-cd ${BASE}/images
-docker-compose -f docker-compose.build.yml build
-
-# Run test docker compose project
-cd ${BASE}/compose/test/
-docker-compose up -d && docker-compose logs -f
+# Build all images and run test docker compose project
+cd ${BASE}/images && docker-compose -f docker-compose.build.yml build && \
+cd ${BASE}/compose/test/ && docker-compose up -d && docker-compose logs -f
