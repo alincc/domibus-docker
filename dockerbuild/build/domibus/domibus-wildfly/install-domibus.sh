82b9658df030f5a554bc3442218db9146cca61b3
Cosmin Baciu <cosmin.baciu@ext.ec.europa.eu>
2017-12-20 16:16:20 +0100

    [EDELIVERY-2950] - Clean the Tomcat Docker scripts

--- dockerbuild/build/domibus/domibus-wildfly/install-domibus.sh
+++ dockerbuild/build/domibus/domibus-wildfly/install-domibus.sh
@@ -27,7 +27,7 @@ function sourceExternalFunctions {
 function configureWildfly {
    displayFunctionBanner ${FUNCNAME[0]}
 
-    unzip -j -o $DOCKER_DOMIBUS_DISTRIBUTION/domibus-distribution-${DOMIBUS_VERSION}-wildfly-full.zip domibus/standalone/configuration/standalone-full.xml -d ${JBOSS_HOME}/standalone/configuration/
+    #unzip -j -o $DOCKER_DOMIBUS_DISTRIBUTION/domibus-distribution-${DOMIBUS_VERSION}-wildfly-full.zip domibus/standalone/configuration/standalone-full.xml -d ${JBOSS_HOME}/standalone/configuration/
 
    ${JBOSS_HOME}/bin/jboss-cli.sh --file=${DOCKER_DOMINSTALL}/wildfly/resources/domibus-wildfly.cli
 }
