409602270e3ce629512a9c07c6a212e3aaa4e55b
FERNANDES Henrique <hfernandes@linkare.com>
2017-12-21 23:26:16 +0000

    EDELIVERY-2969 - Preparing startup script to be used in bamboo... allowing to cloneDomibus, buildDomibus, copyDomibusDistributionImageResources, setDomibusVersion, composeBuildWeblogicClusterImages, copyDomibusConfigurationPolicies and composeUp_C2_C3_WeblogicClusterContainers

--- weblogic-cluster/startup.sh
+++ weblogic-cluster/startup.sh
@@ -1,10 +1,76 @@
 #!/usr/bin/env bash
 
+cloneDomibus() {
+    echo "Clone domibus development branch..."
+    git clone https://ec.europa.eu/cefdigital/code/scm/edelivery/domibus.git domibus --branch ${DOMIBUS_BRANCH} --depth 1
+}
+
+buildDomibus() {
+    echo "Build domibus..."
+    # Official build command for distribution
+    #mvn -f domibus/pom.xml clean install -Ptomcat -Pweblogic -Pwildfly -Pdefault-plugins -Pdatabase -Psample-configuration -PUI -Pdistribution
+    mvn -f domibus/pom.xml clean install -Pweblogic -Pdefault-plugins -Pdatabase -Psample-configuration -PUI -Pdistribution -DskipTests=true -DskipITs=true
+}
+
+setDomibusVersion() {
+    echo "Get domibus version from pom file..."
+    export DOMIBUS_VERSION=$(mvn -f domibus/pom.xml -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
+    export DOMIBUS_SHORT_VERSION=${DOMIBUS_VERSION/-SNAPSHOT/}
+
+    echo "DOMIBUS_VERSION: ${DOMIBUS_VERSION}"
+    echo "DOMIBUS_SHORT_VERSION: ${DOMIBUS_SHORT_VERSION}"
+}
+
+copyDomibusDistributionImageResources() {
+    setDomibusVersion
+
+    echo "Copy domibus distribution artifacts..."
+    ORIGIN_DIST=${BASE}/domibus/Domibus-MSH-distribution/target
+    cp ${ORIGIN_DIST}/domibus-distribution-${DOMIBUS_VERSION}-sql-scripts.zip images/oraclexe-domibus/resources && \
+    cp ${ORIGIN_DIST}/domibus-distribution-${DOMIBUS_VERSION}-default-fs-plugin.zip images/weblogic-cluster-domibus/resources && \
+    cp ${ORIGIN_DIST}/domibus-distribution-${DOMIBUS_VERSION}-default-jms-plugin.zip images/weblogic-cluster-domibus/resources && \
+    cp ${ORIGIN_DIST}/domibus-distribution-${DOMIBUS_VERSION}-default-ws-plugin.zip images/weblogic-cluster-domibus/resources && \
+    cp ${ORIGIN_DIST}/domibus-distribution-${DOMIBUS_VERSION}-sample-configuration-and-testing.zip images/weblogic-cluster-domibus/resources && \
+    cp ${ORIGIN_DIST}/domibus-distribution-${DOMIBUS_VERSION}-weblogic-configuration.zip images/weblogic-cluster-domibus/resources && \
+    cp ${ORIGIN_DIST}/domibus-distribution-${DOMIBUS_VERSION}-weblogic-war.zip images/weblogic-cluster-domibus/resources
+}
+
+copyDomibusConfigurationPolicies() {
+    echo "Copy domibus configuration policies..."
+    ORIGIN_POLICIES=${BASE}/domibus/Domibus-MSH/src/main/conf/domibus/policies
+    DEST_POLICIES=${BASE}/compose/test/common/conf/domibus/policies/
+    cp ${ORIGIN_POLICIES}/doNothingPolicy.xml ${DEST_POLICIES} && \
+    cp ${ORIGIN_POLICIES}/encryptAll.xml ${DEST_POLICIES} && \
+    cp ${ORIGIN_POLICIES}/signEncrypt.xml ${DEST_POLICIES} && \
+    cp ${ORIGIN_POLICIES}/signOnly.xml ${DEST_POLICIES}
+}
+
+composeBuildWeblogicClusterImages() {
+    echo "Compose build Weblogic Cluster Images..."
+    cd ${BASE}/images && docker-compose -f docker-compose.build.yml build
+}
+
+composeUp_C2_C3_WeblogicClusterContainers() {
+    echo "Compose up C2 and C3 Weblogic Cluster Containers..."
+    cd ${BASE}/compose/test/ && docker-compose up -d && docker-compose logs -f
+}
+
+#
+# main
+#
+
+DOMIBUS_BRANCH=development
 BASE=$(pwd)
 
-export DOMIBUS_VERSION=4.0-SNAPSHOT
-export DOMIBUS_SHORT_VERSION=4.0
+# Get domibus
+if [ ! -d "domibus" ]; then
+    cloneDomibus
+    buildDomibus
+    copyDomibusDistributionImageResources
+else
+    setDomibusVersion
+fi
 
-# Build all images and run test docker compose project
-cd ${BASE}/images && docker-compose -f docker-compose.build.yml build && \
-cd ${BASE}/compose/test/ && docker-compose up -d && docker-compose logs -f
+composeBuildWeblogicClusterImages
+copyDomibusConfigurationPolicies
+composeUp_C2_C3_WeblogicClusterContainers
