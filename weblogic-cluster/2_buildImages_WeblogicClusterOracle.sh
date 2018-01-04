6dda9a905fade96431ccd9f2e5d2aab877d8ac42
FERNANDES Henrique <hfernandes@linkare.com>
2018-01-04 15:29:24 +0000

    EDELIVERY-2975 - Copy external image resources from REPO

--- weblogic-cluster/2_buildImages_WeblogicClusterOracle.sh
+++ weblogic-cluster/2_buildImages_WeblogicClusterOracle.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
 source common.sh
+source setEnvironment.sh
+
+copyExternalImageResources() {
+    echo "Copy images external resources..."
+
+    # images/edelivery-weblogic-cluster/resources
+    cp ${REPO}/fmw_12.1.3.0.0_wls.jar images/edelivery-weblogic-cluster/resources && \
+    cp ${REPO}/dockerize-linux-amd64-v0.6.0.tar.gz images/edelivery-weblogic-cluster/resources && \
+    cp ${REPO}/Oracle/Java/jdk-8u144-linux-x64.tar.gz images/edelivery-weblogic-cluster/resources && \
+    # images/weblogic-cluster-domibus/resources
+    cp ${REPO}/Oracle/wslt-api-1.9.1.zip images/weblogic-cluster-domibus/resources
+}
 
 copyDomibusDistributionImageResources() {
     setDomibusVersion
 
     echo "Copy domibus distribution artifacts..."
-    ORIGIN_DIST=domibus/Domibus-MSH-distribution/target
+    local ORIGIN_DIST=domibus/Domibus-MSH-distribution/target
     # oraclexe-domibus/resources
     cp ${ORIGIN_DIST}/domibus-distribution-${DOMIBUS_VERSION}-sql-scripts.zip images/oraclexe-domibus/resources && \
     # weblogic-cluster-domibus/resources
@@ -28,5 +40,6 @@ composeBuildWeblogicClusterImages() {
 #
 export USER_ID=$(id -u $USER)
 
+copyExternalImageResources
 copyDomibusDistributionImageResources
 composeBuildWeblogicClusterImages
