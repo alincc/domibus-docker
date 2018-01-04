169fda5a38b488a97eed73785f7fb81849b9d60f
FERNANDES Henrique <hfernandes@linkare.com>
2018-01-04 17:13:30 +0000

    EDELIVERY-2975 - Using bamboo environment variables

--- weblogic-cluster/setEnvironment.sh
+++ weblogic-cluster/setEnvironment.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-if [ "${DOMIBUS_DOCKER_SET_ENV}" = "true"  ]; then
-    echo "Setting Environment..."
+if [ "${DOMIBUS_DOCKER_LOCAL_ENV}" = "true"  ]; then
+    echo "Using local environment variables..."
     # Image Resources Path
     REPO=~/project/domibus/docker_work/repo
     # Domibus build branch name
     DOMIBUS_BRANCH=development
 else
-    echo "Using external environment"
+    echo "Using bamboo environment variables..."
+    REPO=$bamboo_REPO
+    DOMIBUS_BRANCH=$bamboo_DOMIBUS_BRANCH
 fi
 
 echo "REPO=${REPO}"
