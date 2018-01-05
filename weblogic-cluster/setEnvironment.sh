132673c7dd8fd89c2dc70ae6e3fcedabcb415355
FERNANDES Henrique <hfernandes@linkare.com>
2018-01-05 16:09:32 +0000

    EDELIVERY-2975 - Reverted environment flag

--- weblogic-cluster/setEnvironment.sh
+++ weblogic-cluster/setEnvironment.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
-if [[ -v "${bamboo_buildNumber}" ]]; then
-    # When ${bamboo_buildNumber} is set on the environment
-    echo "Using bamboo environment variables..."
-    REPO=$bamboo_REPO
-    DOMIBUS_BRANCH=$bamboo_DOMIBUS_BRANCH
-else
+if [ "${DOMIBUS_DOCKER_LOCAL_ENV}" = "true"  ]; then
     echo "Using local environment variables..."
 
     # Please set the following variables on your environment
+    # export DOMIBUS_DOCKER_LOCAL_ENV=true
+    #
     # Image External Resources Path, e.g.:
     # export REPO=/datadrive/repo
     #
     # Domibus build branch name, e.g.:
     # export DOMIBUS_BRANCH=development
+else
+    echo "Using bamboo environment variables..."
+    REPO=$bamboo_REPO
+    DOMIBUS_BRANCH=$bamboo_DOMIBUS_BRANCH
 fi
 
 echo "REPO=${REPO}"
