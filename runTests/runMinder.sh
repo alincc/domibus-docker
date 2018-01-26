55a8ac95b31a8a30db3be5065779a3fe959aa693
Ioana DRAGUSANU <idragusa@gmail.com>
2018-01-26 11:18:09 +0200

    Allow time for the suite to run before asking for the status.

--- runTests/runMinder.sh
+++ runTests/runMinder.sh
@@ -83,6 +83,8 @@ function runTests() {
         SUITE_RUN_ID=`runSuite $SUITE_ID`
         echo Result is: $SUITE_RUN_ID
 
+        sleep 600 # allow 10 minutes for the suite to run
+
         # Get suite run status, wait until is ready
         STATUS=`suiteRunStatus $SUITE_RUN_ID`
         echo $STATUS
