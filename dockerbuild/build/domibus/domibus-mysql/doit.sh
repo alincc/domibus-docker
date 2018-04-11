140583fb5128b54e52b42fa72ffa0d514d934e7d
Catalin-Emanuel ENACHE <Catalin-Emanuel.ENACHE@ext.ec.europa.eu>
2018-04-11 14:22:20 +0200

    [EDELIVERY-3147] cleanup of doit.sh files used mostly for debugging

--- dockerbuild/build/domibus/domibus-mysql/doit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd docker/dockerbuild/build/domibus/domibus-mysql
-export DOMIBUS_VERSION=4.0-SNAPSHOT
-DOMIBUS_DISTRIBUTION=/c/Work/Devel/Java/Project/Source/domibus/Domibus-MSH-distribution/target
-./go.sh ${DOMIBUS_DISTRIBUTION}
