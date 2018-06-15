5d890590b28ce2f1c84bdd1716ec68552c8c1300
Cosmin Baciu <cosmin.baciu@ext.ec.europa.eu>
2018-06-15 11:11:43 +0200

    [EDELIVERY-3451] - Added the init data script

--- weblogic-cluster/images/oraclexe-domibus/resources/init_domibus_schema.sql
+++ weblogic-cluster/images/oraclexe-domibus/resources/init_domibus_schema.sql
@@ -6,3 +6,4 @@ grant select on dba_2pc_pending to domibus;
 grant select on dba_pending_transactions to domibus;
 connect domibus/"DOMIBUS_PASSWORD"
 @/docker-entrypoint-initdb.d/oracle10g-DOMIBUS_SHORT_VERSION.ddl
+@/docker-entrypoint-initdb.d/oracle10g-DOMIBUS_SHORT_VERSION-data.ddl
